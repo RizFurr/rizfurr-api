7060b73336e8cd8221d466cc6fec1d1b60334e22
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-12-09 21:17:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { version } = require('./package.json')
+const { version, author } = require('./package.json')
 const binary = require('./lib/binary')
 const iplookup = require('/lib/iplook')
 
-module.exports = { version, binary, iplookup }
+module.exports = { version, author: author.name, binary, iplookup }
