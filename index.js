1e719fa9cd58039fd5cb12776c634dbff0fec692
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-12-10 15:43:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const { version, author } = require('./package.json')
 const binary = require('./lib/binary')
 const iplookup = require('./lib/iplook')
 const artinama = require('./lib/artinama')
+const stress = require('./lib/stress')
 
-module.exports = { version, author: author.name, binary, iplookup, artinama }
+module.exports = { version, author: author.name, binary, iplookup, artinama, stress }
