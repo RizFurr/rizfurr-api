78600c7ad40a81addca1fbd8ca1c5a79bdd2cb1a
caliph01 <admin@caliph.my.id>
2022-04-21 12:35:20 +0700

    Update Infogempa, Pinterest Downloader & Search

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const {
- version,
- author
-} = require('./package.json');
-const binary = require('./lib/binary');
-const iplookup = require('./lib/iplook');
-const artinama = require('./lib/artinama');
-const stress = require('./lib/stress');
-const cuaca = require('./lib/cuaca');
+const { version, author } = require("./package.json");
+const binary = require("./lib/binary");
+const iplookup = require("./lib/iplook");
+const artinama = require("./lib/artinama");
+const stress = require("./lib/stress");
+const cuaca = require("./lib/cuaca");
 const youtube = require("./lib/youtube");
 const fakeua = require("./lib/fakeua");
-const tiktok2 = require('./lib/tiktok2');
-const tiktok = require('./lib/tiktok');
-const igstalk = require('./lib/igstalk');
+const tiktok2 = require("./lib/tiktok2");
+const tiktok = require("./lib/tiktok");
+const igstalk = require("./lib/igstalk");
 const carigrup = require("./lib/grupwa");
 const happymod = require("./lib/happymod");
 const kusonime = require("./lib/kusonime");
@@ -20,8 +17,8 @@ const translate = require("./lib/translate");
 const rangkum = require("./lib/rangkum");
 const film = require("./lib/film");
 const wallpapercave = require("./lib/wallpapercave");
-const family100 = require('./lib/game/family100');
-const soundcloud = require('./lib/soundcloud');
+const family100 = require("./lib/game/family100");
+const soundcloud = require("./lib/soundcloud");
 const whois = require("./lib/tools/whois");
 const getHeaders = require("./lib/tools/headers");
 const shortlink = require("./lib/tools/shortlink");
@@ -40,63 +37,69 @@ const mediafire = require("./lib/mediafire");
 const ttstalk = require("./lib/ttstalk");
 const urlexpand = require("./lib/urlexpand");
 const zippyshare = require("./lib/zippyshare");
+const gempa = require("../lib/infogempa");
+const pindl = require("./lib/pindl");
+const pin = require("./lib/pinsrch");
 
 module.exports = {
- info: {
   version,
-  author: author
- },
- encrypt: {
-  binary
- },
- stress,
- downloader: {
-  youtube,
-  tiktok,
-  tiktok2,
-  soundcloud,
-  instagram,
-  mediafire,
-  zippyshare
- },
- search: {
-  happymod,
-  carigrup,
-  kusonime,
-  iplookup,
-  cuaca,
-  artinama,
-  igstalk,
-  cuaca,
-  film,
-  wallpapercave,
-  singkatankata,
-  persamaankata,
-  quran,
-  freefireid,
-  wall_alphacoders,
-  ttstalk
- },
- random: {
-  fakeua,
-  quotesanime
- },
- other: {
-  emojimix,
-  translate,
-  rangkum
- },
- tools: {
-  whois,
-  getHeaders,
-  shortlink,
-  uploadFile,
-  minifyjs,
-  EncryptHTML,
-  expandurl: urlexpand
- },
- game: {
-  family100,
-  siapakahaku
- }
-}
+  author,
+  info: {
+    gempa,
+  },
+  encrypt: {
+    binary,
+  },
+  stress,
+  downloader: {
+    youtube,
+    tiktok,
+    tiktok2,
+    soundcloud,
+    instagram,
+    mediafire,
+    zippyshare,
+    pindl,
+  },
+  search: {
+    happymod,
+    carigrup,
+    kusonime,
+    iplookup,
+    cuaca,
+    artinama,
+    igstalk,
+    cuaca,
+    film,
+    wallpapercave,
+    singkatankata,
+    persamaankata,
+    quran,
+    freefireid,
+    wall_alphacoders,
+    ttstalk,
+    pin,
+  },
+  random: {
+    fakeua,
+    quotesanime,
+  },
+  other: {
+    emojimix,
+    translate,
+    rangkum,
+  },
+  tools: {
+    whois,
+    getHeaders,
+    shortlink,
+    uploadFile,
+    minifyjs,
+    EncryptHTML,
+    expandurl: urlexpand,
+  },
+  game: {
+    family100,
+    siapakahaku,
+  },
+};
